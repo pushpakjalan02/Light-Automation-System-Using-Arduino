e9b5a93bc2dbbcd4be7aadd68973b9ffff0ef2d2
Pushpak Jalan <pushpakjalan02@gmail.com>
2021-09-17 23:52:20 +0530

    Light Automation System using Arduino

---
+++ Project_Light_Automation_System.ino
@@ -0,0 +1,24 @@
+int flag=0;
+void setup() {
+  // put your setup code here, to run once: 
+  pinMode(9,OUTPUT);
+  pinMode(5,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if((digitalRead(5)==HIGH)&&(flag==0))
+  {
+    delay(1000);
+    digitalWrite(9,HIGH);
+    flag=1;
+  }
+  if((digitalRead(5)==HIGH)&&(flag==1))
+  {
+    delay(1000);
+    digitalWrite(9,LOW);
+    flag=0;
+  }
+  
+}
